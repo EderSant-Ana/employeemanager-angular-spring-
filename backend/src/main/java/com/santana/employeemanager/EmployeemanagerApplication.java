48856bda400a515f80623ce2af25ecc6e79aafda
Eder Sant'ana <edersana@gmail.com>
2021-11-19 17:17:13 -0300

    Profile changed to dev

--- backend/src/main/java/com/santana/employeemanager/EmployeemanagerApplication.java
+++ backend/src/main/java/com/santana/employeemanager/EmployeemanagerApplication.java
@@ -11,9 +11,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import com.santana.employeemanager.model.Employee;
 import com.santana.employeemanager.repo.EmployeeRepo;
-import com.santana.employeemanager.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,25 +25,25 @@ public class EmployeemanagerApplication implements CommandLineRunner {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
 
-	private EmployeeService employeeService;
+	//private EmployeeService employeeService;
 	private EmployeeRepo employeeRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		Employee e1 = Employee.builder().name("Daniel Samuels").email("daniel@gmail.com").jobTitle("JavaScript")
-				.phone("4570090331").imageUrl("https://bootdey.com/img/Content/avatar/avatar1.png").build();
-		Employee e2 = Employee.builder().name("Christine Jones").email("christine@gmail.com").jobTitle("UX/UI")
-				.phone("4570090332").imageUrl("https://bootdey.com/img/Content/avatar/avatar3.png").build();
-		Employee e3 = Employee.builder().name("Dave Rogers").email("dave@gmail.com").jobTitle("Java")
-				.phone("4570090333").imageUrl("https://bootdey.com/img/Content/avatar/avatar7.png").build();
-		Employee e4 = Employee.builder().name("Andy Maxwell").email("andy@gmail.com").jobTitle("SQL")
-				.phone("4570090334").imageUrl("https://bootdey.com/img/Content/avatar/avatar2.png").build();
-
-		employeeService.createEmployee(e1);
-		employeeService.createEmployee(e2);
-		employeeService.createEmployee(e3);
-		employeeService.createEmployee(e4);
+//		Employee e1 = Employee.builder().name("Daniel Samuels").email("daniel@gmail.com").jobTitle("JavaScript")
+//				.phone("4570090331").imageUrl("https://bootdey.com/img/Content/avatar/avatar1.png").build();
+//		Employee e2 = Employee.builder().name("Christine Jones").email("christine@gmail.com").jobTitle("UX/UI")
+//				.phone("4570090332").imageUrl("https://bootdey.com/img/Content/avatar/avatar3.png").build();
+//		Employee e3 = Employee.builder().name("Dave Rogers").email("dave@gmail.com").jobTitle("Java")
+//				.phone("4570090333").imageUrl("https://bootdey.com/img/Content/avatar/avatar7.png").build();
+//		Employee e4 = Employee.builder().name("Andy Maxwell").email("andy@gmail.com").jobTitle("SQL")
+//				.phone("4570090334").imageUrl("https://bootdey.com/img/Content/avatar/avatar2.png").build();
+//
+//		employeeService.createEmployee(e1);
+//		employeeService.createEmployee(e2);
+//		employeeService.createEmployee(e3);
+//		employeeService.createEmployee(e4);
 
 		employeeRepo.findAll().forEach(e -> log.info(e.toString()));
 	}
